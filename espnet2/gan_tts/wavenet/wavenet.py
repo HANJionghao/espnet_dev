a8364c58bd926bea00701ef5f8a222871e0e89a7
“Jionghao” <jionghah@andrew.cmu.ed>
2024-05-12 16:44:56 -0400

    gan_tts: wavenet weight_norm updated

--- espnet2/gan_tts/wavenet/wavenet.py
+++ espnet2/gan_tts/wavenet/wavenet.py
@@ -169,7 +169,7 @@ class WaveNet(torch.nn.Module):
 
         def _apply_weight_norm(m: torch.nn.Module):
             if isinstance(m, torch.nn.Conv1d) or isinstance(m, torch.nn.Conv2d):
-                torch.nn.utils.weight_norm(m)
+                torch.nn.utils.parametrizations.weight_norm(m)
                 logging.debug(f"Weight norm is applied to {m}.")
 
         self.apply(_apply_weight_norm)
